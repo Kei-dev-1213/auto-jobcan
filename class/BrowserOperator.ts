f837a1d183923fc9b3612e6420b2780595a8e465
Kei-dev-1213 <zz2003live@gmail.com>
2025-04-29 18:11:28 +0900

    サンドボックス無効化処理追加

--- class/BrowserOperator.ts
+++ class/BrowserOperator.ts
@@ -17,7 +17,10 @@ export class BrowserOperator {
   private async initialize() {
     try {
       // this.browser = await puppeteer.launch({ headless: false, slowMo: 50 });
-      this.browser = await puppeteer.launch({ slowMo: 50 });
+      this.browser = await puppeteer.launch({
+        slowMo: 50,
+        args: ["--no-sandbox", "--disable-setuid-sandbox"], // サンドボックス無効化
+      });
       this.page = await this.browser.newPage();
       await this._loadPage();
     } catch (e) {
